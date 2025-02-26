d23bdf4848ad0d76a38130537e3af1e34c7e4bc1
Yoganand <yoganandparab@gmail.com>
2025-02-26 23:53:00 +0530

    Added CSV validation

--- src/controller/upload.controller.js
+++ src/controller/upload.controller.js
@@ -1,3 +1,4 @@
+import validateCSVFile from "../service/csv.verify.js";
 import addCSVToQueue from "../service/csvQueue.js";
 
 
@@ -5,11 +6,19 @@ async function uploadCSV(req, res){
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
     }
-    console.log("uploading file");
-    const fileRequestId = await addCSVToQueue(req.file.filename)
+    if(validateCSVFile(req.file.filename,'uploads').valid){
+        console.log("Valid CSV");
+        console.log("uploading file");
+        const fileRequestId = await addCSVToQueue(req.file.filename)
+        console.log(fileRequestId)
+        res.status(200).json(fileRequestId);
+    }
+    else{
+        console.log("Invalid CSV");
+        return res.status(400).send('Invalid CSV file.');
+        
+    }
 
-    console.log(fileRequestId)
-    res.status(200).json(fileRequestId);
 
 }
 
